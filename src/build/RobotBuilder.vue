f5042e0413226146116b4c954f1baee9f467a807
richie.buckingham <richie.buckingham@gmail.com>
2018-07-17 20:43:51 +1000

    display shopping cart using v-for

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
+  <div class="content">
+    <button
+      class="add-to-cart"
+      @click="addToCart()">
+      Add to cart...
+    </button>
     <div class="top-row">
       <div class="top part">
         <div class="robot-name">
           {{selectedRobot.head.title}}
+          <span v-if="selectedRobot.head.onSale"
+                class="sale">Sale!</span>
         </div>
         <img :src="selectedRobot.head.src" title="head"/>
         <button @click="selectPreviousHead()" class="prev-selector">&#9668;</button>
@@ -34,6 +41,23 @@
         <button @click="selectNextBase()" class="next-selector">&#9658;</button>
       </div>
     </div>
+        <div>
+      <h1>Cart</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Robot</th>
+            <th class="cost">Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(robot, index) in cart" :key="index">
+            <td>{{robot.head.title}}</td>
+            <td class="cost">{{robot.cost}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <div>
       <pre>
       {{selectedRobot}}
@@ -62,6 +86,7 @@ export default {
   data() {
     return {
       availableParts,
+      cart: [],
       selectedHeadIndex: 0,
       selectedBaseIndex: 0,
       selectedLeftArmIndex: 0,
@@ -81,6 +106,20 @@ export default {
     },
   },
   methods: {
+    addToCart() {
+      // calculate the cost of the robot, and add it to an array
+      const robot = this.selectedRobot;
+      const cost =
+        robot.head.cost +
+        robot.leftArm.cost +
+        robot.rightArm.cost +
+        robot.torso.cost +
+        robot.base.cost;
+      console.log('cost = ', cost);
+
+      this.cart.push(Object.assign({}, robot, { cost }));
+      console.log('this.cart[0] = ', this.cart[0]);
+    },
     selectNextHead() {
       this.selectedHeadIndex = getNextValidIndex(
         this.selectedHeadIndex,
@@ -244,5 +283,27 @@ export default {
   top: -25px;
   text-align: center;
   width: 100%;
+}
+.sale {
+  color: red;
+}
+.content {
+  position: relative;
+}
+.add-to-cart {
+  position: absolute;
+  right: 30px;
+  width: 220px;
+  padding: 3px;
+  font-size: 16px;
+}
+td,
+th {
+  text-align: left;
+  padding: 5px;
+  padding-right: 20px;
+}
+.cost {
+  text-align: right;
 }
 </style>
